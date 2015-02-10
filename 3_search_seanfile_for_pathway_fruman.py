93259a9d0d4b113abc20e8c452ba2510f5a94c9d
sophietrefely <s.trefely@gmail.com>
2015-02-10 12:11:58 +1100

    Processed redundant uniprots using eval
    ‘eval’ was used to extract the redundant list of uniprots.
    
    There is a problem with repetition in the output

--- 3_search_seanfile_for_pathway_fruman.py
+++ 3_search_seanfile_for_pathway_fruman.py
@@ -1,34 +1,35 @@
-## Need to extract all the data from 'sean_list_converted.txt' for the proteins in the KEGG glycolysis/gluconeogenesis pathway. 
+# Need to extract all the data from 'sean_list_converted.txt' for the proteins in the KEGG glycolysis/gluconeogenesis pathway. 
 # opened file to read:
-path_to_sean_list_converted = 'data/sean_list_converted.txt'
-sean_list_converted = open(path_to_sean_list_converted, 'rU') #'rU' avoids confusion with read and formatting /r
+path_to_sean_list_converted_redundant = 'data/sean_list_converted_redundant.txt'
+sean_list_converted_redundant = open(path_to_sean_list_converted_redundant, 'rU') #'rU' avoids confusion with read and formatting /r
 #turn txt file into a list:
+# each entry in list will look like:
+# ['37165', "['Q6Y681', 'Q63891', 'Q61994', 'Q9WTZ5', 'Q9WTZ6', 'Q7TSC9', 'Q7TSD0', 'Q8C4C1', 'Q99JF4', 'P25425', 'Q99JH0', 'Q8BT04', 'Q8K570', 'Q9WTZ4']", 'SGGTSSSPIKAIF', '-0.13', '-0.05', '0.14', '0.06', '-0.07', '0.1', '0.8', '-0.12', '0.81', 'NaN', 'NaN', 'NaN', '-0.13', '-0.05', 'NaN', 'NaN', 'NaN', 'NaN', '0.14', 'NaN', 'NaN', '0.06', 'NaN', 'NaN', '-0.07', 'NaN', 'NaN', '0.10', '0.51', 'NaN', '1.08', 'NaN', 'NaN', '-0.12', 'NaN', '1.05', '0.56', 'NaN', 'NaN', 'NaN']
 sean_converted_list = []
-for line in sean_list_converted:
+for line in sean_list_converted_redundant:
     stripped_line = line.rstrip()   # remove the newline from the line, it has one by default
     sean_split = stripped_line.split('\t') #fields are tab seperated
     sean_converted_list.append(sean_split)
 print("loaded sean's data")
 
-## fetch the uniprot IDs for fructose/mannose pathway in 'fruman_uniprots.txt'
+# fetch the uniprot IDs for fructose/mannose pathway in 'fruman_uniprots.txt'
 # opened file to read
 path_to_fruman_uniprots = 'data/fruman_uniprots.txt'
 fruman_uniprots_file = open(path_to_fruman_uniprots, 'rU')
-fruman_uniprots = set(fruman_uniprots_file.read().strip().split('\n'))  # this makes a set rather than a list so in the next step the search for each entry searches the whole set, where there could be multiple entries, rather than stopping when finding one as in a  list 
+fruman_uniprots = fruman_uniprots_file.read().strip().split('\n')
 print(fruman_uniprots)
 
 ## Export all fruman gene data into a new txt file ('glyoclysis_data.txt'):
 path_to_fruman_data = 'data/fruman_data.txt'
 fruman_data = open(path_to_fruman_data, 'w')
 
-# Each item looks like ['37165', 'Q9WTZ4', 'SGGTSSSPIKAIF', ..., <lots of numbers>]
+# Each item looks like ['37165', "['Q6Y681', 'Q63891', 'Q61994', 'Q9WTZ5']" , 'SGGTSSSPIKAIF', ..., <lots of numbers>]
+# ie ['number', "['uniprot a', 'uniprot b']", 'sequence', 'number', ....]
 for item in sean_converted_list:
-    sean_uniprots = item[1]
-    sean_uniprot_split = sean_uniprots.split(' ') #split the multiple space separated uniprots into seperate elements
-    for element in sean_uniprot_split:
-        uniprot = element
-    #if uniprot is in the KEGG fructose mannose list print all data relating to that protein in sean_list to file
-        if uniprot in fruman_uniprots: #this is a set not a list so can find multiple entries not th efirst only
+    sean_uniprots = eval(item[1])  #eval unpacks the values  in the "[]". 
+    for sean_uniprot in sean_uniprots: #go through each uniprot in redundant list   
+#if uniprot is in the KEGG fructose mannose list print all data relating to that protein in sean_list to file
+        if sean_uniprot in fruman_uniprots: #this is list so only uses the first entry - stops repetition as sean_conversion_list is redundant for all possible uniprots
             item_for_write = '\t'.join(item) + '\n' #join is the opposite of split. split by tab before therefore must join by tab and can then add newline as string because it is no longer a set/list
             fruman_data.write(item_for_write)
 fruman_data.close()
---
+++ unwrap_list_str_list.py
@@ -0,0 +1,15 @@
+list1 = [['orange',"['horse', 'cat', 'dog']", 'pear'], ['lemon', "['cow', 'sheep']", 'orange']]
+
+##for element in list1:
+##    animals = element[1]
+##    animals_strip = animals.strip('[]')
+##    animals_list = animals_strip.split(',')
+##    for animal in animals_list:
+##        animal_strip = animal.strip()
+##        print(animal_strip)
+    
+for element in list1:
+    animals = eval(element[1])
+    for animal in animals:
+        print(str(animal))
+    
